d5db41c9f3440cec436e909a890957753cca4d79
Akash Raje <akash.raje@gmail.com>
2019-12-07 15:30:51 +0530

    terraform create instance

--- workshop/ec2-instance.tf
+++ workshop/ec2-instance.tf
@@ -4,9 +4,9 @@ resource "aws_key_pair" "ssh_key" {
 }
 
 resource "aws_instance" "first_instance" {
-  ami = ""
+  ami = "ami-04b9e92b5572fa0d1"
 
-  subnet_id = ""
+  subnet_id = "subnet-16979c38"
 
   key_name = aws_key_pair.ssh_key.key_name
 
